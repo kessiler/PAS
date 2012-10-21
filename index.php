c807e8831f4710c7ffb6feffde819f50eececd7b
kessiler <kessipredador@hotmail.com>
2012-10-21 19:07:51 -0200

    Classe de PDF para gerar relatórios. (FPDF.class.php)

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 1);
 require_once('autoload.php');
 define("SESSION_NAME", "c21f969b5f03d33d43e04f8f136e7682");
-define('CORE', dirname(__FILE__).'/modules/classes/');
+define('CORE', dirname(__FILE__).DIRECTORY_SEPARATOR.'modules'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR);
 define('TEMPLATE', 'default/');
 define('TITLE_SITE', 'SICG - Sistema integrado de cadastro e gerenciamento');
 /*
@@ -21,6 +21,9 @@ define('HOST', '127.0.0.1');
 define('USER', 'root');
 define('PASS', '123');
 define('BD', 'webpas');
+
+/* Definição do diretório de fontes para PDF */
+define('FPDF_FONTPATH', CORE.DIRECTORY_SEPARATOR.'font');
 
 ob_start();
 session_name(SESSION_NAME);
---
+++ modules/classes/RelacDietas.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: KESSILER
+ * Date: 21/10/12
+ * Time: 18:30
+ * To change this template use File | Settings | File Templates.
+ */
+if (!class_exists('RelacDietas')) {
+
+    class RelacDietas extends MySQL
+    {
+
+    }
+}
